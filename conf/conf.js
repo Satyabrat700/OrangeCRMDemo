c3f0049cfa690b46dba2c1d627de075292af55dc
Satyabrata <satyabrat700@gmail.com>
2021-05-22 13:53:50 +0200

    22nd may

--- conf/conf.js
+++ conf/conf.js
@@ -1,135 +1,139 @@
-
-var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+var HtmlScreenshotReporter = require("protractor-jasmine2-screenshot-reporter");
+var HtmlReporter = require("protractor-beautiful-reporter");
 
 var reporter = new HtmlScreenshotReporter({
-  dest: 'target/screenshots',
-  filename: 'my-report.html'
+  dest: "target/screenshots",
+  filename: "my-report.html",
 });
-
 
 // An example configuration file.
 exports.config = {
-
   // Framework to use. Jasmine is recommended.
-  framework: 'jasmine',
+  framework: "jasmine",
 
-    //directConnect: true,
-  seleniumAddress: 'http://10.10.20.235:4460/wd/hub',
+  directConnect: true,
+  seleniumAddress: "http://10.10.20.235:4460/wd/hub",
 
   // Capabilities to be passed to the webdriver instance.
-  
+
   capabilities: {
-    'browserName': 'chrome' // providing the browser name ::::::Demo for GIT
-  
+    browserName: "chrome", // providing the browser name ::::::Demo for GIT
   },
-
-
-  
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['..\\tests\\orangeCRMDEMO.js'],
+  specs: ["..\\tests\\orangeCRMDEMO.js"],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 30000,
   },
-  beforeLaunch: function() {
-    return new Promise(function(resolve){
+  beforeLaunch: function () {
+    return new Promise(function (resolve) {
       reporter.beforeLaunch(resolve);
     });
   },
 
   // Assign the test reporter to each running instance
-  onPrepare: async function() {
- 
+  onPrepare: async function () {
     await browser.waitForAngularEnabled(false);
-    var jasmineReporters = require('jasmine-reporters');
-    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-    consolidateAll: true,
-    savePath: './',
-    filePrefix: 'xmlresults'
-    
-}));
+    var jasmineReporters = require("jasmine-reporters");
+    jasmine.getEnv().addReporter(
+      new jasmineReporters.JUnitXmlReporter({
+        consolidateAll: true,
+        savePath: "./",
+        filePrefix: "xmlresults",
+      })
+    );
     jasmine.getEnv().addReporter(reporter);
-    var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-    }));
+    var AllureReporter = require("jasmine-allure-reporter");
+    jasmine.getEnv().addReporter(
+      new AllureReporter({
+        resultsDir: "allure-results",
+      })
+    );
 
-    var fs = require('fs-extra');
- 
-fs.emptyDir('screenshots/', function (err) {
-        console.log(err);
+    // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+    jasmine.getEnv().addReporter(
+      new HtmlReporter({
+        baseDirectory: "tmp/screenshots",
+      }).getJasmine2Reporter()
+    );
+
+    var fs = require("fs-extra");
+
+    fs.emptyDir("screenshots/", function (err) {
+      console.log(err);
     });
- 
+
     jasmine.getEnv().addReporter({
-        specDone: function(result) {
-            if (result.status == 'failed') {
-                browser.getCapabilities().then(function (caps) {
-                    var browserName = caps.get('browserName');
- 
-                    browser.takeScreenshot().then(function (png) {
-                        var stream = fs.createWriteStream('screenshots/' + browserName + '-' + result.fullName+ '.png');
-                        stream.write(new Buffer(png, 'base64'));
-                        stream.end();
-                    });
-                });
-            }
+      specDone: function (result) {
+        if (result.status == "failed") {
+          browser.getCapabilities().then(function (caps) {
+            var browserName = caps.get("browserName");
+
+            browser.takeScreenshot().then(function (png) {
+              var stream = fs.createWriteStream(
+                "screenshots/" + browserName + "-" + result.fullName + ".png"
+              );
+              stream.write(new Buffer(png, "base64"));
+              stream.end();
+            });
+          });
         }
+      },
     });
   },
 
   // Close the report after all tests finish
-  afterLaunch: function(exitCode) {
-    return new Promise(function(resolve){
+  afterLaunch: function (exitCode) {
+    return new Promise(function (resolve) {
       reporter.afterLaunch(resolve.bind(this, exitCode));
     });
   },
 
-  onComplete: function() {
-     var browserName, browserVersion;
-     var capsPromise = browser.getCapabilities();
- 
-     capsPromise.then(function (caps) {
-        browserName = caps.get('browserName');
-        browserVersion = caps.get('version');
- 
-        var HTMLReport = require('protractor-html-reporter');
- 
-        testConfig = {
-            reportTitle: 'Test Execution Report',
-            outputPath: './',
-            screenshotPath: './screenshots',
-            testBrowser: browserName,
-            browserVersion: browserVersion,
-            modifiedSuiteName: false,
-            screenshotsOnlyOnFailure: true
-        };
-        new HTMLReport().from('xmlresults.xml', testConfig);
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get("browserName");
+      browserVersion = caps.get("version");
+
+      var HTMLReport = require("protractor-html-reporter");
+
+      testConfig = {
+        reportTitle: "Test Execution Report",
+        outputPath: "./",
+        screenshotPath: "./screenshots",
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+      };
+      new HTMLReport().from("xmlresults.xml", testConfig);
     });
- },
- onComplete: function() {
-  var browserName, browserVersion;
-  var capsPromise = browser.getCapabilities();
+  },
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
 
-  capsPromise.then(function (caps) {
-     browserName = caps.get('browserName');
-     browserVersion = caps.get('version');
+    capsPromise.then(function (caps) {
+      browserName = caps.get("browserName");
+      browserVersion = caps.get("version");
 
-     var HTMLReport = require('protractor-html-reporter');
+      var HTMLReport = require("protractor-html-reporter");
 
-     testConfig = {
-         reportTitle: 'Test Execution Report',
-         outputPath: './',
-         screenshotPath: './screenshots',
-         testBrowser: browserName,
-         browserVersion: browserVersion,
-         modifiedSuiteName: false,
-         screenshotsOnlyOnFailure: true
-         
-     };
-     new HTMLReport().from('xmlresults.xml', testConfig);
- });
-}
+      testConfig = {
+        reportTitle: "Test Execution Report",
+        outputPath: "./",
+        screenshotPath: "./screenshots",
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+      };
+      new HTMLReport().from("xmlresults.xml", testConfig);
+    });
+  },
 };
--- pages/LoginAndVerifyLeave.js
+++ pages/LoginAndVerifyLeave.js
@@ -1,63 +1,64 @@
-
-    let LoginAndVerifyLeave = function () {
-
-    let welcomeButton=element(by.id("welcome"));
-    let logOutLink=element(by.linkText("Logout"));
-    let userName = element(by.id('txtUsername'));
-    let password = element(by.id('txtPassword'));
-    let loginButton = element(by.id('btnLogin'));
-    let menu_leave_viewLeaveModule=element(by.xpath("//a[@id='menu_leave_viewLeaveModule']/b"));
-    let menu_leave_viewMyLeaveList=element(by.id("menu_leave_viewMyLeaveList"));
-    let calFromDate=element(by.id("calFromDate"));
-    let calToDate=element(by.id("calToDate"));
-    let btnSearch=element(by.id("btnSearch"))
-    let textTobeValidated=element(by.xpath("//table[@id='resultTable']/tbody/tr/td[2]"));
-    this.logOut = async function () {
-          try {
-	                await welcomeButton.click();
-                  browser.driver.sleep(2000);
-		              await logOutLink.click();
-
-
-          } catch (error) {
-                console.log(error+'occured inside logOut block');
-
-               }
+let LoginAndVerifyLeave = function () {
+  let welcomeButton = element(by.id("welcome"));
+  let logOutLink = element(by.linkText("Logout"));
+  let userName = element(by.id("txtUsername"));
+  let password = element(by.id("txtPassword"));
+  let loginButton = element(by.id("btnLogin"));
+  let menu_leave_viewLeaveModule = element(
+    by.xpath("//a[@id='menu_leave_viewLeaveModule']/b")
+  );
+  let menu_leave_viewMyLeaveList = element(by.id("menu_leave_viewMyLeaveList"));
+  let calFromDate = element(by.id("calFromDate"));
+  let calToDate = element(by.id("calToDate"));
+  let btnSearch = element(by.id("btnSearch"));
+  let textTobeValidated = element(
+    by.xpath("//table[@id='resultTable']/tbody/tr/td[2]")
+  );
+  this.logOut = async function () {
+    try {
+      await welcomeButton.click();
+      browser.driver.sleep(2000);
+      await logOutLink.click();
+    } catch (error) {
+      console.log(error + "occured inside logOut block");
     }
+  };
 
-   this.loginAndVerify = async function (UserID,UserPassword,startDate,endDate,empName,URL) {
-          try {
-       
-            desiredOption = userName;
-            EC = protractor.ExpectedConditions;
-            browser.wait(EC.visibilityOf(desiredOption), 5000);
-            await desiredOption.click();
-	          await userName.sendKeys(UserID);
-		        await password.sendKeys(UserPassword);
+  this.loginAndVerify = async function (
+    UserID,
+    UserPassword,
+    startDate,
+    endDate,
+    empName,
+    URL
+  ) {
+    try {
+      desiredOption = userName;
+      EC = protractor.ExpectedConditions;
+      browser.wait(EC.visibilityOf(desiredOption), 5000);
+      await desiredOption.click();
+      await userName.sendKeys(UserID);
+      await password.sendKeys(UserPassword);
 
-            await loginButton.click();
+      await loginButton.click();
 
-	        	await menu_leave_viewLeaveModule.click();
-	        	await menu_leave_viewMyLeaveList.click();
-	
+      await menu_leave_viewLeaveModule.click();
+      await menu_leave_viewMyLeaveList.click();
 
-		        await calFromDate.click();
-            await calFromDate.clear();
-	        	await calFromDate.sendKeys(startDate);
-             await btnSearch.click();
-             
-            await calToDate.click();
-            await calToDate.clear();
-	          await calToDate.sendKeys(endDate);
-		        await btnSearch.click();
-            expect(await textTobeValidated.getText()).toContain(empNameVerify);
+      await calFromDate.click();
+      await calFromDate.clear();
+      await calFromDate.sendKeys(startDate);
+      await btnSearch.click();
 
-              } catch (error) {
-                console.log(error+'occured inside loginAndVerify block');
-
-              }
-
+      await calToDate.click();
+      await calToDate.clear();
+      await calToDate.sendKeys(endDate);
+      await btnSearch.click();
+      expect(await textTobeValidated.getText()).toContain(empNameVerify);
+      //return  textTobeValidated.getText();
+    } catch (error) {
+      console.log(error + "occured inside loginAndVerify block");
     }
- 
+  };
 };
 module.exports = new LoginAndVerifyLeave();
